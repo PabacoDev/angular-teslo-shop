b5b64042709aa573dea3adf86c98b993ed831c35
Francisco Barragán Cortés <francisco.barragan@sofitec.aero>
2025-05-16 12:28:16 +0200

    Pruebas despliegue

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -8,7 +8,6 @@ import { delay, forkJoin, map, Observable, of, switchMap, tap } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { Product } from '../interfaces/product.interface';
 import { User } from '@auth/interfaces/auth-response.interface';
-import { formatCurrency, formatDate } from '@angular/common';
 
 const baseUrl = environment.baseUrl;
 
@@ -57,6 +56,7 @@ export class ProductsService {
       })
       .pipe(
         tap((resp) => console.log(resp)),
+        tap((resp) => console.log(`${baseUrl}`)),
         tap((resp) => this.productsCache.set(key, resp))
       );
   }
